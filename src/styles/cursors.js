0f4d35be3270b7251caec12f519d94417993f933
Andrew Gallagher <andrewtgallagherJS@gmail.com>
2022-05-31 16:31:16 -0400

    remove cursors

--- src/styles/cursors.js
+++
@@ -1,5 +0,0 @@
-import defaultSrc from "assets/images/cursors/default.png";
-
-export const cursors = {
-	default: defaultSrc,
-};
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,5 @@
 import { breakpoints } from "styles/breakpoints";
 import { colors } from "styles/colors";
-import { cursors } from "styles/cursors";
 import { fontFamily } from "styles/fontFamily";
 import { fontSize } from "styles/fontSize";
 import { shadows } from "styles/shadows";
@@ -8,7 +7,6 @@ import { shadows } from "styles/shadows";
 export const theme = {
 	breakpoints,
 	colors,
-	cursors,
 	fontFamily,
 	fontSize,
 	shadows,
