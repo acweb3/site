eab4288e63b69007717a075dae25508b4a209d4b
Andrew Gallagher <andrewtgallagherJS@gmail.com>
2022-06-02 12:43:27 -0400

    hide mobile translation

--- src/components/SiteWrapper/BlurWrapper/BlurWrapper.styled.js
+++ src/components/SiteWrapper/BlurWrapper/BlurWrapper.styled.js
@@ -7,16 +7,25 @@ export const BlurWrapperContent = styled.div`
 		props.lock !== undefined &&
 		css`
 			filter: blur(6px);
-			transform: translateY(-${props.lock}px);
 			pointer-events: none;
 		`};
+
+	${(props) => props.theme.breakpoints.extraSmall`
+		${(props) =>
+			props.lock !== undefined &&
+			css`
+				transform: translateY(-${props.lock}px);
+			`};
+	`}
 `;
 
 export const BlurWrapper = styled.div`
-	${(props) =>
-		props.lock !== undefined &&
-		css`
-			max-height: 100vh;
-			overflow: hidden;
-		`};
+	${(props) => props.theme.breakpoints.extraSmall`
+		${(props) =>
+			props.lock !== undefined &&
+			css`
+				max-height: 100vh;
+				overflow: hidden;
+			`};
+	`}
 `;
